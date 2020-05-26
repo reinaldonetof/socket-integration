1589347d1c2dd8e9f45f62bdd4a5036ea4f69f3f
Reinaldo Neto <reinaldonetof@hotmail.com>
2020-05-26 19:28:33 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => res.render("index"));
+
+app.get("/sendmsg", (req, res) => {
+  io.emit("msg", req.query);
+  res.send({ ok: true });
+});
+
+app.get("/sendmsg", (req, res) => {
+  io.emit("msg", req.query);
+  res.send({ ok: true });
+});
+
+http.listen(3000, () => {
+  console.log("Socket.io integration...");
+});
